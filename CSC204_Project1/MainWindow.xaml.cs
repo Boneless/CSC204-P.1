4f3a2722fb96552d4470c7a32acbeb71e3afff58
dre3520 <dre3520@gmail.com>
2015-01-20 16:05:16 -0500

    test comment

--- CSC204_Project1/MainWindow.xaml.cs
+++ CSC204_Project1/MainWindow.xaml.cs
@@ -21,6 +21,7 @@ namespace CSC204_Project1
     /// </summary>
     public partial class MainWindow : Window
     {
+        //test comment
         public MainWindow()
         {
             InitializeComponent();
